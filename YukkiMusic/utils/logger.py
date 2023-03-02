264a18448bb25e110c4985eaadbb616200beb85e
Raichuop07 <126665079+Raichuop07@users.noreply.github.com>
2023-03-02 10:49:57 +0530

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -12,7 +12,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**YUKKI PLAY LOG**
+**NINJA PLAY LOG**
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
 **User:** {message.from_user.mention}
